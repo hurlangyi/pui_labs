db74542e9faac9aebd0292708f94072103397110
hurlangyi <rachellee0699@gmail.com>
2021-03-28 17:15:00 -0400

    Week 8 Lab Changes

--- week7_lab/main.js
+++ week7_lab/main.js
@@ -32,6 +32,21 @@ function getRandomIndex(maxIndex) {
     return Math.floor(Math.random() * maxIndex);
 }
 
+function generateRandomAnimal() {
+    var randomIndex = getRandomIndex(zooAnimals.length);
+    var randomAnimal = zooAnimals[randomIndex];
+
+    if (randomAnimal instanceof panda) {
+        return new panda(generateRandomName(), generateRandomAge());
+    }
+    else if (randomAnimal instanceof crocodile) {
+        return new crocodile(generateRandomName(), generateRandomAge());
+    }
+    else if (randomAnimal instanceof peacock) {
+        return new peacock(generateRandomName(), generateRandomAge());
+    }
+}
+
 function generateRandomName() {
     let randomIndex = getRandomIndex(names.length);
     return names[randomIndex];
@@ -59,10 +74,34 @@ function generateRandomAnimal() {
 }
 
 function onLoad() {
-    var animal = generateRandomAnimal();
-    console.log(animal);
+    var animal = JSON.parse(localStorage.getItem("savedAnimal"));
+    var hasSavedAnimal = false;
+
+    if (animal === null) {
+        document.getElementById("button-storage").textContent = "Save Animal";
+        animal = generateRandomAnimal();
+    }
+    else {
+        document.getElementById("button-storage").textContent = "Clear Animal";
+        hasSavedAnimal = true;
+    }
+
     document.getElementById("animal-properties").textContent = animal.name + ", " + animal.age + " years old";
-    let imageTag = document.getElementById("animal-img");
-    imageTag.setAttribute("src", animal.image);
-    imageTag.setAttribute("alt", animal.image_alt);
-}
+    document.getElementById("animal-img").setAttribute("src", animal.image);
+
+    document.getElementById("button-storage").addEventListener("click", function() {
+        if (hasSavedAnimal) {
+            localStorage.removeItem("savedAnimal");
+            document.getElementById("button-storage").style.display = "none";
+            document.getElementById("button-action-text").textContent = "Cleared";
+            document.getElementById("button-action-text").style.display = "block";
+        }
+
+        else {
+            localStorage.setItem("savedAnimal", JSON.stringify(animal));
+            document.getElementById("button-storage").style.display = "none";
+            document.getElementById("button-action-text").textContent = "Saved!";
+            document.getElementById("button-action-text").style.display = "block";
+        }
+    });
+};
